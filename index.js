aa7f58b14d880f17d525a99626c88b6197d52c04
buldukmustafa36 <buldukmustafa36@gmail.com>
2026-06-11 19:37:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const mineflayer = require('mineflayer');
+const http = require('http');
+
+// Render'ın botu uyutmaması için gereken kısım
+http.createServer((req, res) => res.end('Bot aktif!')).listen(process.env.PORT || 3000);
+
+const bot = mineflayer.createBot({
+  host: 'dynamic-6.magmanode.com:25732', 
+  port: 25565,
+  username: 'Bot'
+});
+
+bot.on('spawn', () => {
+  console.log('Bot sunucuya girdi!');
+});
+
+bot.on('kicked', console.log);
+bot.on('error', console.log);
